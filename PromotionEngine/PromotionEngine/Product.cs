93e8d38e9e86bf8960adb0795caa4d01a190c0f9
Chhotumal Ingole <chhotumal.ingole@gmail.com>
2020-09-04 11:44:46 +0530

    SKU Product list added

---
+++ PromotionEngine/PromotionEngine/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine
+{
+    class Product
+    {
+        public string SKUName { get; set; }
+        public int SKUPrice { get; set; }
+    }
+}
--- PromotionEngine/PromotionEngine/Program.cs
+++ PromotionEngine/PromotionEngine/Program.cs
@@ -10,6 +10,27 @@ namespace PromotionEngine
     {
         static void Main(string[] args)
         {
+
+            List<Product> products = new List<Product>
+            {
+                new Product{ SKUName="A", SKUPrice=50 },
+                new Product{ SKUName="B", SKUPrice=30 },
+                new Product{ SKUName="C", SKUPrice=20 },
+                new Product{ SKUName="D", SKUPrice=15 },
+
+            };
+
+            Console.WriteLine("***********************Promotion Engine***********************");
+            Console.WriteLine("***********************SKU List***********************");
+
+            Console.WriteLine("\n");
+
+            foreach (var item in products)
+            {
+                Console.WriteLine("SKU: " + item.SKUName + " Price: " + item.SKUPrice);
+            }
+
+            Console.ReadKey();
         }
     }
 }
